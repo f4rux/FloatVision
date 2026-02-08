614908671d7c6c1b38a7241e3e9b9261a46782aa
f4rux <faruxz@protonmail.com>
2026-02-08 21:35:51 +0900

    Fix WM_DESTROY case block for timer handler

---
+++ third_party/md4c/md4c.c
@@ -0,0 +1,11 @@
+#include "md4c.h"
+
+int md_parse(const MD_CHAR* text, MD_SIZE size, MD_OUTPUT_FUNC output, void* userdata, unsigned parser_flags)
+{
+    (void)text;
+    (void)size;
+    (void)output;
+    (void)userdata;
+    (void)parser_flags;
+    return 0;
+}
